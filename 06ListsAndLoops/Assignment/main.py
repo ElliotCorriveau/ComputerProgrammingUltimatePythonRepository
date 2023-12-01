1b152ed3e508a0a8cb5b3b2b378b54900c3887c5
ElliotCorriveau <26elliotcorriveau@usd266.com>
2023-12-01 09:43:53 -0600

    finished new exercises

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -4,6 +4,7 @@ def count_failing_grades(grades):
         if grade < 60:
             count = count + 1 
             return count
+print("count_failing_grades --------------------------------------")
 print(count_failing_grades([70, 59, 60]))
 
 
@@ -13,6 +14,7 @@ def count_act_scores(scores):
         if score > 0 and score < 37:
             count = count + 1
     return count
+print("count_act_scores --------------------------------------")
 print(count_act_scores([1, 36, 37, 0, 5]))
 
 
@@ -21,6 +23,7 @@ def number_sum(numbers):
     for number in numbers:
         total = total + number
     return total
+print("number_sum --------------------------------------")
 print(number_sum([1, 2, 3, 4]))
 
 
@@ -33,5 +36,53 @@ def average_act_score(scores):
             total = score + total
     mean = total / count
     return mean
+print("average_act_score --------------------------------------")
 print(average_act_score([1, 17, 37]))
 
+
+def all_true(booleans):
+    count  = 0
+    for boolean in booleans:
+        if boolean == True:
+            count = count + 1
+    if count == len(booleans):
+        return True
+    else:
+        return False
+print("all_true --------------------------------------")   
+print(all_true([True, False, False]))
+print(all_true([True, True, True]))
+
+def any_true(booleans):
+    for boolean in booleans:
+        if boolean == True:
+            return True
+    else:
+        return False
+print("any_true --------------------------------------")
+print(any_true([True, False, True]))
+print(any_true([False, False, False]))
+
+
+def has_vowel(vowels):
+    for vowel in vowels:
+        if vowel in ["a", "e", "i", "o", "u"]:
+            return True
+        else:
+            return False
+print("has_vowel --------------------------------------")
+print(has_vowel(["a", "e", "i", "o", "u"]))
+print(has_vowel(["a,", "e", "i", "o", "r"]))
+
+def mostly_true(booleans):
+    count = 0
+    for boolean in booleans:
+        if boolean == True:
+            count = count + 1
+    if count > len(booleans) / 2: 
+        return True
+    else:
+        return False
+print("mostly_true --------------------------------------")
+print(mostly_true([True, False, True]))
+print(mostly_true([False, False, True]))
